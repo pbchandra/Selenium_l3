991affb533db8e82adad567d89df6379cf5c229d
Bhuvana Chandra <55701451+pbchandra@users.noreply.github.com>
2020-06-27 14:55:15 +0530

    Add files via upload

---
+++ helpingfiles/Constant.java
@@ -0,0 +1,12 @@
+package helpingfiles;
+
+public class Constant {
+
+	
+	public static final String newtours_URL="http://newtours.demoaut.com/";
+	public static final String Store_URL="http://automationpractice.com/index.php";
+	public static final String practice_URL="https://letskodeit.teachable.com/p/practice";
+	public static final String Path_TestData ="src\\\\testdata\\";
+	public static final String File_TestData = "TestData.xlsx";
+	
+}
---
+++ helpingfiles/excel_helper.java
@@ -0,0 +1,99 @@
+package helpingfiles;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class excel_helper {
+
+	private static XSSFSheet ExcelWSheet;
+
+	private static XSSFWorkbook ExcelWBook;
+
+	private static XSSFCell Cell;
+
+	private static XSSFRow Row;
+
+	public static void setExcelFile(String Path, String SheetName) throws Exception {
+
+		try {
+
+			FileInputStream ExcelFile = new FileInputStream(Path);
+
+			ExcelWBook = new XSSFWorkbook(ExcelFile);
+
+			ExcelWSheet = ExcelWBook.getSheet(SheetName);
+
+		} catch (Exception e) {
+
+			throw (e);
+
+		}
+
+	}
+
+	public static String getCellData(int RowNum, int ColNum) throws Exception {
+
+		try {
+
+			DataFormatter formatter = new DataFormatter(); // creating formatter using the default locale
+			Cell = ExcelWSheet.getRow(RowNum).getCell(ColNum);
+
+			String CellData = formatter.formatCellValue(Cell);
+
+			return CellData;
+
+		} catch (Exception e) {
+
+			return "Failed";
+
+		}
+
+	}
+
+//This method is to write in the Excel cell, Row num and Col num are the parameters
+
+	public static void setCellData(String Result, int RowNum, int ColNum) throws Exception {
+
+		try {
+
+			Row = ExcelWSheet.getRow(RowNum);
+
+			Cell = Row.getCell(ColNum);
+
+			if (Cell == null) {
+
+				Cell = Row.createCell(ColNum);
+
+				Cell.setCellValue(Result);
+
+			} else {
+
+				Cell.setCellValue(Result);
+
+			}
+
+			// Constant variables Test Data path and Test Data file name
+
+			FileOutputStream fileOut = new FileOutputStream(Constant.Path_TestData + Constant.File_TestData);
+
+			ExcelWBook.write(fileOut);
+
+			fileOut.flush();
+
+			fileOut.close();
+
+		} catch (Exception e) {
+
+			throw (e);
+
+		}
+
+	}
+
+}
